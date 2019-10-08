79c001b1873d5f07e4b0073f37295a5543060cbd
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-10-08 07:03:02 +0200

    add snake button

---
+++ src/components/snakeButton.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from 'react';
+import styles from './snakeButton.module.sass';
+
+export const SnakeButton: FunctionComponent = () => {
+  return (
+    <button className={styles.button}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Button
+    </button>
+  );
+};
+
+export default SnakeButton;
