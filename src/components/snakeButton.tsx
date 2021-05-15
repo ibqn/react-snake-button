d53279de9c0bb12a295f9d0cb3a6e12cf03cd542
ibqn <evgen.ibqn@gmail.com>
2021-05-15 22:16:13 +0200

    remove unneeded imports

--- src/components/snakeButton.tsx
+++ src/components/snakeButton.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from 'react';
-import styles from './snakeButton.module.sass';
+import { FunctionComponent } from "react";
+import styles from "./snakeButton.module.sass";
 
 export const SnakeButton: FunctionComponent = () => {
   return (
