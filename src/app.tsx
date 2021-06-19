7db2d990d074254ff8797d8ce9250e83bca8044a
ibqn <evgen.ibqn@gmail.com>
2021-06-19 11:37:30 +0200

    simplify code

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import './app.sass'
+import { SnakeButton } from './components/snake-button'
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <SnakeButton />
+    </header>
+  </div>
+)
+
+export default App
---
+++ src/components/snake-button.tsx
@@ -0,0 +1,13 @@
+import styles from './snake-button.module.sass'
+
+export const SnakeButton = () => (
+  <button className={styles.button}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    Button
+  </button>
+)
+
+export default SnakeButton
